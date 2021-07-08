95c0d45b33ea49a33bbec79d560c8de1d33704cc
John Maruska <jackmaruska@gmail.com>
2021-07-08 16:27:46 -0500

    Add create and update deploy commands to infra.sh

--- infra.sh
+++ infra.sh
@@ -3,6 +3,8 @@ VM_NAME=cromwell
 DB_INSTANCE=cromwell1
 DEPLOY_NAME=cromwell
 
+SRC_DIR="$(dirname "${BASH_SOURCE[0]}")"
+
 case $1 in
     "start")
         echo "Starting database $DB_INSTANCE"
@@ -16,4 +18,11 @@ case $1 in
         echo "Stopping database $DB_INSTANCE"
         gcloud sql instances patch $DB_INSTANCE --activation-policy NEVER
         ;;
+    "create-deploy")
+        echo "Creating new deployment"
+        gcloud deployment-manager deployments create $DEPLOY_NAME --config $SRC_DIR/jinja/deployment.yaml
+        ;;
+    "update-deploy")
+        echo "Updating previous deployment"
+        gcloud deployment-manager deployments update $DEPLOY_NAME --config $SRC_DIR/jinja/deployment.yaml
 esac
