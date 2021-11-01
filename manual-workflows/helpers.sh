9c682f900efd49bc329e7af57e751e28397c2794
John Maruska <jackmaruska@gmail.com>
2021-11-01 17:10:04 -0500

    Rewrite save_timing_diagram for resiliency
    Now exit early and properly clean up in typical failure scenarios

--- manual-workflows/helpers.sh
+++ manual-workflows/helpers.sh
@@ -17,7 +17,6 @@ function refresh_zip_deps () {
     cd $OLD_DIR
 }
 
-# TODO: add resiliency.  This assumes no issues (1) starting server
 function save_timing_chart () {
     WORKFLOW_ID=$1
     GCS_PATH=$2
@@ -25,18 +24,23 @@ function save_timing_chart () {
         echo "Usage: save_timing_chart WORKFLOW_ID GCS_PATH"
     else
         # Server start-up and destruction
-        cromwell server > cromwell.log &
+        cromwell server > cromwell.log & > /dev/null
         CROMWELL_PID=$!
-        trap 'kill $CROMWELL_PID $(($CROMWELL_PID + 1)) $(($CROMWELL_PID + 2)) && echo KILLED_TRAP' EXIT
 
-        echo "Started cromwell server on pid [$CROMWELL_PID]"
-        ( tail -f cromwell.log & ) | grep -q 'service started on'
-        echo "Cromwell server completed startup"
-        curl --fail http://localhost:8000/api/workflows/v1/$WORKFLOW_ID/timing > ${WORKFLOW_ID}_timing.html || \
-            echo "Request for timing diagram on workflow $WORKFLOW_ID failed."
-        if [[ -s ${WORKFLOW_ID}_timing.html ]]; then
-            gsutil cp $GCS_PATH ${WORKFLOW_ID}_timing.html
+        echo "Instantiating cromwell server on pid [$CROMWELL_PID]"
+        RESULT=$(( tail -f cromwell.log & ) | grep -oh -m 1 'service started\|Shutting down')
+        if [[ $RESULT = "Shutting down" ]]; then
+            echo "Cromwell failed to instantiate. View error logs at cromwell.log"
+            kill  $CROMWELL_PID $(pgrep -P $CROMWELL_PID)
+        else
+            echo "Cromwell server completed startup"
+            curl --fail http://localhost:8000/api/workflows/v1/$WORKFLOW_ID/timing > ${WORKFLOW_ID}_timing.html
+            if [[ $? -ne 0 ]]; then
+                echo "Request for timing diagram on workflow $WORKFLOW_ID failed."
+            else
+                gsutil cp $GCS_PATH ${WORKFLOW_ID}_timing.html
+            fi
+            kill $CROMWELL_PID $(pgrep -P $CROMWELL_PID)
         fi
-        kill $CROMWELL_PID $(($CROMWELL_PID + 1)) $(($CROMWELL_PID + 2))
     fi
 }
