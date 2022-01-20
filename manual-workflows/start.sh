65b7d19435a0c39909f7d700bfa82ff4e3c62c42
John Maruska <jackmaruska@gmail.com>
2022-01-20 13:41:24 -0600

    Allow caller to specify machine type for manual workflows VM

--- manual-workflows/start.sh
+++ manual-workflows/start.sh
@@ -6,9 +6,10 @@ $0 - Start a new Cromwell VM instance
 usage: $0 INSTANCE_NAME [--argument value]*
 
 arguments:
--h, --help             print this block and immediately exits
---server-account      Email identifier of service account used by main Cromwell instance
---cromwell-conf        Local path to configuration file for Cromwell server
+-h, --help         print this block and immediately exits
+--server-account   Email identifier of service account used by main Cromwell instance
+--cromwell-conf    Local path to configuration file for Cromwell server. DEFAULT $SRC_DIR/cromwell.conf
+--machine-type     GCP machine type for the instance. DEFAULT e2-standard-2
 EOF
 }
 
@@ -41,6 +42,14 @@ while test $# -gt 0; do
                 shift
             fi
             ;;
+        --machine-type*)
+            if [ ! "$2" ]; then
+                die 'ERROR: "--machine-type" requires a string argument.'
+            else
+                MACHINE_TYPE=$2
+                shift
+            fi
+            ;;
         *)
             break
             ;;
@@ -48,12 +57,14 @@ while test $# -gt 0; do
     shift
 done
 
+MACHINE_TYPE=${MACHINE_TYPE:-"e2-standard-2"}
 [ -z $SERVER_ACCOUNT ] && die "Missing argument --server-account"
 
 CROMWELL_CONF="$SRC_DIR/cromwell.conf"
 if [[ ! -f $CROMWELL_CONF ]]; then
     cat <<EOF
 cromwell.conf does not exist. Check passed value or generate via
+
     sh resources.sh generate-cromwell-conf --project PROJECT --bucket BUCKET
 
 EOF
@@ -64,18 +75,21 @@ gcloud compute instances create $INSTANCE_NAME \
        --image-family debian-11 \
        --image-project debian-cloud \
        --zone us-central1-c \
-       --machine-type=e2-standard-2 \
+       --machine-type=$MACHINE_TYPE \
        --service-account=$SERVER_ACCOUNT --scopes=cloud-platform \
        --network=default --subnet=default \
        --metadata=cromwell-version=71 \
        --metadata-from-file=startup-script=$SRC_DIR/server_startup.py,cromwell-conf=$CROMWELL_CONF,helpers-sh=$SRC_DIR/helpers.sh,cromwell-service=$SRC_DIR/cromwell.service
 
-echo "To use this instance, SSH into it via:"
-echo ""
-echo "    gcloud compute ssh $INSTANCE_NAME"
-echo ""
-echo "To delete the instance when you're done:"
-echo ""
-echo "    gcloud compute instances delete $INSTANCE_NAME"
-echo ""
+cat <<EOF
+To use this instance, SSH into it via:
+
+    gcloud compute ssh $INSTANCE_NAME
+
+To delete the instance when you're done:
+
+    gcloud compute instances delete $INSTANCE_NAME
+
+EOF
 exit 0
+n
