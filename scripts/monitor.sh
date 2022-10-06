a954fa56a26e68f5a9ca26e19060e8ef2ce6aab6
Malachi Griffith <malachig@gmail.com>
2022-10-06 15:59:26 -0500

    additional updates

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -1,9 +1,13 @@
 #! /bin/bash
 
-printf "Memory_Percent\tMemory_Percent_Peak\tMemory_GB\tMemory_GB_Peak\tDisk_Percent\tDisk_G\tCPU_Percent\n"
+printf "Seconds\tMemory_Percent\tMemory_Percent_Peak\tMemory_GB\tMemory_GB_Peak\tDisk_Percent\tDisk_Percent_Peak\tDisk_GB\tDisk_GB_Peak\tCPU_Percent\tCPU_Percent_Peak\n"
 DELAY=5
 MEMORY_P_PEAK=0
 MEMORY_G_PEAK=0
+DISK_P_PEAK=0
+DISK_G_PEAK=0
+CPU_P_PEAK=0
+i=0
 
 while : 
 do
@@ -18,20 +22,23 @@ do
 
   #Retrieve current disk usage as percent and GB (assumes disk used by cromwell is mounted as '/mnt/disks/local-disk')
   DISK_P=$(df -h | awk '$NF=="/"{printf "%s", $5}' | tr -d "%" | awk '{printf "%.2f", $1}')
-  DISK_G=$(df -h | awk '$NF=="/"{printf "%s", $4}' | tr -d "BKMGTP")
+  DISK_G=$(df -B 1M | awk '$NF=="/"{printf "%.2f", $4/1024}')
 
   #Check for and store peak disk usage
+  DISK_P_PEAK=$(echo $DISK_P $DISK_P_PEAK | awk '{if ($1 > $2) print $1; else print $2}')
+  DISK_G_PEAK=$(echo $DISK_G $DISK_G_PEAK | awk '{if ($1 > $2) print $1; else print $2}')
 
   #Retrieve overall current CPU usage
   CPU_P=$(top -bn1 | grep load | awk '{printf "%.2f", $(NF-2)}')
 
+  #Check for and store peak CPU usage
+  CPU_P_PEAK=$(echo $CPU_P $CPU_P_PEAK | awk '{if ($1 > $2) print $1; else print $2}')
+
   #Print out the data line
-  echo -e "$MEMORY_P\t$MEMORY_P_PEAK\t$MEMORY_G\t$MEMORY_G_PEAK\t$DISK_P\t$DISK_G\t$CPU_P"
+  echo -e "$i\t$MEMORY_P\t$MEMORY_P_PEAK\t$MEMORY_G\t$MEMORY_G_PEAK\t$DISK_P\t$DISK_P_PEAK\t$DISK_G\t$DISK_G_PEAK\t$CPU_P\t$CPU_P_PEAK"
 
   sleep $DELAY
+  let "i+=$DELAY"
 
 done
-
-
-
 
